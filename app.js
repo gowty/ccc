8cd1e9b32f64c35be663fee2e4e259602a6cd687
gowty <867573@gmail.com>
2017-10-03 19:24:54 +0530

    ccc Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 7000;
+
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+  res.sendfile(__dirname + "/public/index.html");
+});
+
+app.listen(port, function() {
+  console.log("server started");
+});
